dc79bfe7348c17cc7f2750a387a63e43e53286b0
masonrosauer-art <masonrosauer@seattleacademy.org>
2025-10-21 21:58:48 +0000

    hey josh i changed the colors and backround

---
+++ Projects/image.py
@@ -0,0 +1,26 @@
+import turtle 
+turtle.Screen().bgcolor("pink")
+t = turtle.Turtle()
+t.speed(10)
+
+t.goto(0,0)
+colors = ["black","dark blue","crimson"]
+
+for i in range (150): 
+    t.color(colors[ i % 3])
+
+    t.forward (100)
+    t.left (90) 
+    t.forward (80)
+    t.left (45) 
+    t.forward (75)
+    t.left (50)    
+    t.forward (105)
+    t.left (95) 
+    t.forward (85)
+    t.left (55) 
+    t.forward (85)
+    t.left (60)
+
+
+turtle.exitonclick()
